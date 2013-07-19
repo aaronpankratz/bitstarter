e3982a04181f65e196e63c948fe41e0d03e78c59
Ubuntu <ubuntu@domU-12-31-39-06-91-DE.compute-1.internal>
2013-07-19 04:52:33 +0000

    last commit take 2

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fileBuf = new Buffer(fs.readFileSync('index.html'));
+    response.send(fileBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
