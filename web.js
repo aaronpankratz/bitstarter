5109ae6cb216465dbf9dc9da540dde31a56d1665
Aaron Pankratz <pankratz.aaron@gmail.com>
2013-07-26 04:55:42 +0000

    change web.js port = 8080

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var express = require('express');
+var express = require('./node_modules/express');
 var app = express();
 app.use(express.logger());
 
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
     response.send(fileBuf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
